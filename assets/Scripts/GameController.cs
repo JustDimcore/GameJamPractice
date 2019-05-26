2b937af789ea5db28f5e56fed68b2e7acb25c2bd
Dmitriy Ilchenko <d.ilchenko@plarium.com>
2019-05-26 12:02:15 +0300

    GameController god object

---
+++ assets/Scripts/GameController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void InitGame()
+    {
+        ClearOldGame();
+        StartNewGame();
+    }
+
+    private void ClearOldGame()
+    {
+        // TODO: Clear old objects and states
+        
+        // TODO: Remove characters
+        // TODO: Remove mobs
+        // TODO: Remove body parts
+        // TODO: Disable laser
+        // TODO: Clear statistics
+    }
+
+    private void StartNewGame()
+    {
+        // TODO: Create characters
+        // TODO: Start mobs spawning
+        // TODO: Start laser spawning
+    }
+
+    private void RecreateCharacter(int playerIndex)
+    {
+        // TODO: Remove old/disable controlling
+        // TODO: Create new
+    }
+
+    private void SpawnMob()
+    {
+        // TODO: Create path
+        // TODO: Create mob
+    }
+    
+    // Remove when he comes to door
+    private void OnMobExit()
+    {
+        // TODO: Remove character, which comes to door
+        // TODO: Create new one in another
+    }
+}
